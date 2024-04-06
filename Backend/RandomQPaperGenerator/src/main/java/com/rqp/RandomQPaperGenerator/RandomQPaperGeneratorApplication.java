5e4c3a967d3d5a698f51927791f74e36d3ce2915
shamanthhegde2 <shamanthhegde12@gmail.com>
2024-04-05 21:03:11 -0500

    spring boot project

---
+++ Backend/RandomQPaperGenerator/src/main/java/com/rqp/RandomQPaperGenerator/RandomQPaperGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.rqp.RandomQPaperGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RandomQPaperGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RandomQPaperGeneratorApplication.class, args);
+	}
+
+}
---
+++ Backend/RandomQPaperGenerator/src/test/java/com/rqp/RandomQPaperGenerator/RandomQPaperGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rqp.RandomQPaperGenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RandomQPaperGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
